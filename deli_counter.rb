6e74346800838092872a21226aeb343c45f18fef
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-22 01:17:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ katz_deli = []
 
 def line(current_line)
   if(current_line.size > 0)
-    current_line.each_with_index do do |person, index|
+    current_line.each_with_index do |person, index|
       puts "The line is currently: #{index+1}. #{person}"
     end
   else
